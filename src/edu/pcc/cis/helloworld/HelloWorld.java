69fa7178732412dc8cfca80f61eb8d9dc90dacff
David Kastelic <david.kastelic@pcc.edu>
2023-10-17 11:36:31 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by David Kastelic on 10.17.23
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
